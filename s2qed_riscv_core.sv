c7523d8d46d58fff17538ad4a44baa0fe54a2010
Silvio Heverton Campelo de Santana <santana@frascati.eit.uni-kl.de>
2020-09-17 18:54:52 +0200

    add merged pipeline properties and s2qed properties

--- s2qed_riscv_core.sv
+++ s2qed_riscv_core.sv
@@ -1,43 +1,7 @@
+// Copy of the top level riscv_core used for the
+// s2qed verification
 
-// Copyright 2018 ETH Zurich and University of Bologna.
-// Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the "License"); you may not use this file except in
-// compliance with the License.  You may obtain a copy of the License at
-// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
-// or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
-
-////////////////////////////////////////////////////////////////////////////////
-// Engineer:       Matthias Baer - baermatt@student.ethz.ch                   //
-//                                                                            //
-// Additional contributions by:                                               //
-//                 Igor Loi - igor.loi@unibo.it                               //
-//                 Andreas Traber - atraber@student.ethz.ch                   //
-//                 Sven Stucki - svstucki@student.ethz.ch                     //
-//                 Michael Gautschi - gautschi@iis.ee.ethz.ch                 //
-//                 Davide Schiavone - pschiavo@iis.ee.ethz.ch                 //
-//                                                                            //
-// Design Name:    Top level module                                           //
-// Project Name:   RI5CY                                                      //
-// Language:       SystemVerilog                                              //
-//                                                                            //
-// Description:    Top level module of the RISC-V core.                       //
-//                 added APU, FPU parameter to include the APU_dispatcher     //
-//                 and the FPU                                                //
-//                                                                            //
-////////////////////////////////////////////////////////////////////////////////
-
-import apu_core_package::*;
-
-`include "include/riscv_config.sv"
-
-import riscv_defines::*;
-import top_level_types::*;
-//import isa_ri5cy_types::*;
-
-module riscv_core
+module s2qed_riscv_core
 #(
   parameter N_EXT_PERF_COUNTERS =  0,
   parameter INSTR_RDATA_WIDTH   = 32,
